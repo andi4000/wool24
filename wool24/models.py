bb1518c2a8153cecb0912645e89b3515d2568b8f
ay <aybenggolo@gmail.com>
2023-06-02 17:40:12 +0200

    remove unused fat libraries

--- wool24/models.py
+++ wool24/models.py
@@ -3,7 +3,6 @@
 from typing import Optional
 
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel
 
 
 class UpstreamProductInfo(BaseModel):
@@ -16,20 +15,3 @@ class UpstreamProductInfo(BaseModel):
     composition: Optional[str] = None
     brand: Optional[str] = None
     model: Optional[str] = None
-
-
-class Product(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    fullname: str = Field(index=True)
-    brand: Optional[str]
-    model: Optional[str]
-
-
-class ProductDetails(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    product_id: Optional[int] = Field(default=None, foreign_key="product.id")
-    price: float
-    available: bool
-    needle_size: Optional[str]
-    composition: Optional[str]
-    vendor_product_url: str
