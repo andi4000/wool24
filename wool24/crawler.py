d9bf00e3103b5820aefbe7af95a832d01196e283
ay <aybenggolo@gmail.com>
2023-06-02 17:40:12 +0200

    add main entry point for the crawler

---
+++ wool24/crawler.py
@@ -0,0 +1,36 @@
+# pylint: disable=missing-docstring
+import logging
+from typing import Optional
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from wool24.models import UpstreamProductInfo
+from wool24.scrapers import ProductNotFoundException, WebsiteScraper
+
+
+class Crawler:
+    def __init__(self) -> None:
+        self.driver: Optional[WebDriver] = None
+
+    def scrape(self, keyword: str) -> list[UpstreamProductInfo]:
+        """Scrape keyword and write to database."""
+        logging.info("Booting up browser instance")
+        opts = Options()
+        opts.headless = True
+        self.driver = webdriver.Firefox(options=opts)
+
+        scraper_results: list[UpstreamProductInfo] = []
+        for scraper in WebsiteScraper.scrapers:
+            inst = scraper(self.driver)
+            try:
+                product_details = inst.get_product_details(keyword)
+                scraper_results.append(product_details)
+            except ProductNotFoundException:
+                pass
+
+        self.driver.close()
+        logging.info("Browser instance closed.")
+
+        return scraper_results
---
+++ wool24/main.py
@@ -0,0 +1,44 @@
+# pylint: disable=missing-docstring
+import argparse
+import logging
+import sys
+from io import TextIOWrapper
+
+from wool24.crawler import Crawler
+
+KEYWORDS_MIN_LENGTH = 4
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Scrape wool prices from various vendors"
+    )
+    parser.add_argument("keywords", type=str, help="Search keywords i.e. product name")
+    parser.add_argument(
+        "-o",
+        "--out",
+        nargs="?",
+        type=argparse.FileType("w"),
+        default=sys.stdout,
+        help="Output file. Writes to stdout if not defined.",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Turn on verbosity"
+    )
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+
+    if len(args.keywords) < KEYWORDS_MIN_LENGTH:
+        logging.error("Search keyword too short.")
+        sys.exit()
+
+    keywords: str = args.keywords
+    outfile: TextIOWrapper = args.out
+
+    crawler = Crawler()
+    results = crawler.scrape(keywords)
+    for result in results:
+        outfile.write(result.json())
+        outfile.write("\n")
